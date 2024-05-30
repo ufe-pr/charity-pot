23f4aa4b9cd7747d8247da8dd82b738b3f60576e
Ufedojo Atabo <ufedojoatabo@gmail.com>
2024-05-30 02:30:08 +0100

    fix build error

--- src/app/ai-chat.tsx
+++ src/app/ai-chat.tsx
@@ -2,12 +2,16 @@
 
 import { FLOWISE_FLOW_ID, FLOWISE_URL } from "@/config/constants";
 import { BubbleChat } from "flowise-embed-react";
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function AIChat() {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  useLayoutEffect(() => {
+  const [windowWidth, setWindowWidth] = useState(0);
+  useEffect(() => {
     window.onresize = (e) => setWindowWidth(window.innerWidth);
+    window.onload = (e) => setWindowWidth(window.innerWidth);
+    if (window) {
+      setWindowWidth(window.innerWidth);
+    }
   }, []);
   return (
     <BubbleChat
